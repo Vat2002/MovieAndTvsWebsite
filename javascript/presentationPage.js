960c55df0eea88f34cac930754bfe8ab8bbff7e0
nadundanushka696969 <naudunstudy2020@gmail.com>
2021-06-28 17:01:20 +0530

    presentaionpage is 1/2 done.

---
+++ javascript/presentationPage.js
@@ -0,0 +1,13 @@
+const names=["name1","name2","name3","name4"];
+names.forEach(printNames);
+function printNames(val,index){
+	srtNames+="<h4 id=\"name"+ index+"\">"+val+"</h4>";
+}
+document.getElementById("displayNames").innerHTML=srtNames;
+function animation(){
+	document.getElementById("displayNames").classList.remove("position");
+	document.getElementById("displayNames").classList.add("positionchange");
+	document.getElementById("title").classList.add("positionchange");
+}
+setTimeout(animation,4000);
+setTimeout(function(){location.href="homepage.html"},4200);
