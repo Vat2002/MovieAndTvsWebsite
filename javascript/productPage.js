cc9a32123fbdc56a2f326862571e118ee52cb11c
nadundanushka696969 <naudunstudy2020@gmail.com>
2021-07-03 21:08:41 +0530

    new button

--- javascript/productPage.js
+++ javascript/productPage.js
@@ -4,13 +4,6 @@ const productList=[
 	["name2",24,"15/10/2020","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
 	["name3",50,"05/01/1999","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
 	["name4",78,"03/01/2000","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
-	["name5",12,"25/09/2013","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
-	["name5",12,"25/09/2013","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
-	["name5",12,"25/09/2013","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
-	["name5",12,"25/09/2013","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
-	["name5",12,"25/09/2013","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
-	["name5",12,"25/09/2013","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
-	["name5",12,"25/09/2013","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""],
 	["name5",12,"25/09/2013","ksdgkjkfkfkagsdkhfgakyusgdckjvasdyugakjsdgfk",""]
 ];
 
@@ -28,9 +21,31 @@ for (let i=0;i<productList.length;i++){
 	"<img src=\""+imglocation+"\"class=\"imgWidth\">"+
 	"<div class=\"container\">Name : "+name+"<br>"+
 	"Price : $"+price+"<br>"+
-	"Release Date : "+releasedate+"<br></div></div>";
+	"Release Date : "+releasedate+"<br>"+
+	"<button id=\"addtocart\" style=\"margin: 10px;\" value=\""+i+"\" onclick=\"add2cart(this.value)\">Add to cart</button>"+
+	"</div></div>";
 	//"Description : "+description+"</br>";
+	//console.log(document.getElementById("addtocart").value);
 };
 
+//var productid;
+var incartlist=[];
 //document.getElementById("showProduct").innerHTML=productList;
+function add2cart(id){
+	//productid=parseInt(document.getElementById("addtocart").value);
+	incartlist.push(parseInt(id));
+	console.log(id);
+	//console.log(incartlist);
+	//alert(id);
+}
+list="";
+function printcart(){
+	for (let i=0;i<incartlist.length;i++){
+		list+=productList[incartlist[i]]+"\n";
+	}
+	alert(incartlist+"\n"+list);
+}
+//var productid=document.getElementById("add2cart").name;
+//alert(productid);
+//document.getElementById("addtocart").addEventListener("click",add2cart)
 document.getElementById("showProduct").innerHTML=displayProduct;
