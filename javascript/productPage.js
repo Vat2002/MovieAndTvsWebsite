8f5b73fe380154d64f927d2583bb1bb6fc1fc4a1
nadundanushka696969 <naudunstudy2020@gmail.com>
2021-07-10 15:37:47 +0530

    add new features

--- javascript/productPage.js
+++ javascript/productPage.js
@@ -29,23 +29,60 @@ for (let i=0;i<productList.length;i++){
 };
 
 //var productid;
-var incartlist=[];
+const incartlist=[];
+var totalPrice=0;
 //document.getElementById("showProduct").innerHTML=productList;
 function add2cart(id){
+	var name=productList[id][0];
+	var price=productList[id][1];
+	var imglocation=productList[0][4];
+	var item;
+	item="<tr><td width=\"20%\"><img src=\""+imglocation+"\"></td>"+
+	"<td style=\"\">Name : "+name+
+	"<priceoncard>$"+price+"<br></priceoncard>"+
+	/*"<br><button onclick=\"\" style=\"float: right;\">remove</button></td>"+*/
+	"</tr>";
+	totalPrice+=price;
 	//productid=parseInt(document.getElementById("addtocart").value);
-	incartlist.push(parseInt(id));
-	console.log(id);
+	incartlist.push(item);
+	console.log(id,price);
 	//console.log(incartlist);
 	//alert(id);
 }
-list="";
+
 function printcart(){
-	for (let i=0;i<incartlist.length;i++){
-		list+=productList[incartlist[i]]+"\n";
+	list=incartlist.valueOf();
+	console.log(incartlist);
+	if(incartlist.length>0){
+		document.getElementById("incartitem").innerHTML=incartlist.join("");
+		document.getElementById("placeorderbtn").style.display = "block";
+		document.getElementById("price").innerHTML="$"+totalPrice;
+	}else{
+		document.getElementById("incartitem").innerHTML="<h1 style=\"text-align: center;\">Cart is empty.<h1>";
+		document.getElementById("placeorderbtn").style.display = "none";
 	}
-	alert(incartlist+"\n"+list);
+	document.getElementById("overlay").style.display = "block";
+}console.log(incartlist);
+
+/*function on() {
+  document.getElementById("incartitem overlay").style.display = "block";
+}*/
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
 }
+
+function resetthecart(){
+	while(incartlist.length>0){
+		incartlist.pop();
+	}
+	window.totalPrice=0;
+	printcart();
+}
+
 //var productid=document.getElementById("add2cart").name;
 //alert(productid);
 //document.getElementById("addtocart").addEventListener("click",add2cart)
 document.getElementById("showProduct").innerHTML=displayProduct;
+//document.getElementById("incartitem").innerHTML=incartlist.join("");
+//document.getElementById("incartitem").innerHTML=list;
